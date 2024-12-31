8cbaf35012c9a3f54005e8e81b229ed48fcf59e3
Brandon Lee Truter <brandon.truter@ncino.com>
2024-12-31 07:32:46 +0200

    allow users to access links and votes

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -1,9 +1,9 @@
-# frozen_string_literal: true
-
 module Types
-  class UserType < Types::BaseObject
+  class UserType < BaseObject
     field :id, ID, null: false
     field :name, String, null: false
     field :email, String, null: false
+    field :votes, [VoteType], null: false
+    field :links, [LinkType], null: false
   end
 end
